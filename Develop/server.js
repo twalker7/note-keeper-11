28e6bf484273b6aec216f58124fedba7ff9098f9
Thomas Walker <thomasnwalker@yahoo.com>
2021-07-04 23:38:11 -0700

    Add Heroku

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,34 @@
 const notes = require('./db/db.json');
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 443;
+
+
+
+// homepage 
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname,  './public/index.html'))
+});
+
+//notes page 
+app.get('/notes', (req, res)=>{
+    res.sendFile(path.join(__dirname,  './public/notes.html'))
+});
+
+
+app.get('/api/notes', (req, res)=>{
+
+});
+
+
+app.post('/api/notes', (req, res)=>{
+
+});
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`API server now on port ${PORT}!`);
+});
