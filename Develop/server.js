cf7aa6cd0a7c4c4ac0cce10860f9490327dda074
Thomas Walker <thomasnwalker@yahoo.com>
2021-07-04 23:43:08 -0700

    middleware functions

--- Develop/server.js
+++ Develop/server.js
@@ -5,6 +5,12 @@ const path = require('path');
 const PORT = process.env.PORT || 443;
 
 
+// Necessary middleware functions ... parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+
 
 // homepage 
 app.get('/', (req, res)=>{
